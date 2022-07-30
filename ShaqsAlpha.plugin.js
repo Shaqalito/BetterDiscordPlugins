99074f713e0fa9c7d306dfca944a0775b55486ce
Shaqalito <ferriisaie@gmail.com>
2022-07-31 00:00:41 +0200

    Version changes check

--- ShaqsAlpha.plugin.js
+++ ShaqsAlpha.plugin.js
@@ -2,7 +2,7 @@
  * @name ShaqsAlpha
  * @author Shaqalito's Labs
  * @description Alpha version of all Shaqs Plugins.
- * @version a0.0.2
+ * @version a0.0.1
  * @invite j2VFQVjWGN
  * @authorId 370576698481180674
  * @authorLink https://github.com/shaqalito
@@ -32,7 +32,7 @@ const defaultConfig = [
         "value": false
     }
     ]
-const config = {"main": "index.js", "info": {"name": "ShaqsAlpha", "authors": [{"name": "Shaqalito's Labs", "discord_id": "370576698481180674", "github_username": "Shaqalito"}], "description": "Alpha version of all Shaqs Plugins.", "version": "a0.0.2", "github_raw": "https://github.com/Shaqalito/BetterDiscordPlugins/blob/main/ShaqsAlpha.plugin.js?raw=true", "github": "https://github.com/shaqalito"}, "changelog": changelog, "defaultConfig": defaultConfig}
+const config = {"main": "index.js", "info": {"name": "ShaqsAlpha", "authors": [{"name": "Shaqalito's Labs", "discord_id": "370576698481180674", "github_username": "Shaqalito"}], "description": "Alpha version of all Shaqs Plugins.", "version": "a0.0.1", "github_raw": "https://github.com/Shaqalito/BetterDiscordPlugins/blob/main/ShaqsAlpha.plugin.js?raw=true", "github": "https://github.com/shaqalito"}, "changelog": changelog, "defaultConfig": defaultConfig}
 
 
 module.exports = (() => {
@@ -64,7 +64,7 @@ module.exports = (() => {
 
   return class ShaqsAlpha extends Plugin {
     onStart() {
-        PluginUpdater.checkForUpdate("ShaqsAlpha", this.config.info.version, this.config.info.github_raw)
+        PluginUpdater.checkForUpdate("ShaqsAlpha", this._config.info.version, this._config.info.github_raw)
         const voiceModule = WebpackModules.getByPrototypes("setSelfDeaf");
         Patcher.after(voiceModule.prototype, "initialize", this.replacement.bind(this));
     }
