ed6b85f6328775d0bede26e80e7d148cd6d41a13
Shaqalito <ferriisaie@gmail.com>
2022-07-30 21:51:18 +0200

    initial commit

---
+++ plugin.plugin.js
@@ -0,0 +1,19 @@
+/**
+ * @name ShaqsAlpha
+ * @author Shaqalito's Labs
+ * @description Alpha version of all Shaqs Plugins.
+ * @version a0.0.1
+ * @invite j2VFQVjWGN
+ * @authorId 370576698481180674
+ * @authorLink https://github.com/shaqalito
+ * @updateUrl 
+ */
+
+module.exports = class ExamplePlugin {
+    load() {} // Optional function. Called when the plugin is loaded in to memory
+
+    start() {} // Required function. Called when the plugin is activated (including after reloads)
+    stop() {} // Required function. Called when the plugin is deactivated
+
+    observer(changes) {} // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
+}
